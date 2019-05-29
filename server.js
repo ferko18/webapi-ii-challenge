8ac299111af06d38790571e9e703bbf251669c26
Firealem Erko <firealemg@yahoo.com>
2019-05-29 11:50:24 -0400

    added routes and CRUD end points

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express');
-
-
-
 const server = express();
 
+const postRouter = require('./data/postRouter')
+
+server.use('/api/posts', postRouter);
+
+server.get('/', (req, res) => {
+  res.send(`
+  <h2>welcome to your second api</h2>
+  `)
+})
 
 module.exports = server;
